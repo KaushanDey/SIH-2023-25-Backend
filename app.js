b1b8bf240de3e20d92558646cf507c580434c33e
KaushanDey <kaushandey2003@yahoo.com>
2023-09-27 10:09:29 +0530

    cors added

--- app.js
+++ app.js
@@ -13,12 +13,7 @@ app.use(express.json());
 app.use(`/user`,userRouter);
 app.use(`/news`,newsRouter);
 app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  next();
-});
+app.options('*',cors());
 
 const start = async () => {
   try {
