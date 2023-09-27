c1f431da9fd013f06cbf1ac5013d672beb388f24
KaushanDey <kaushandey2003@yahoo.com>
2023-09-27 10:30:16 +0530

    msg

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -1,14 +1,9 @@
 import express from "express";
 import { getNews, postNews } from "../controllers/newsController.js";
-import cors from "cors";
+
 
 const newsRouter = express.Router();
-app.use(cors({
-    origin:"*",
-    methods:['GET','POST','OPTIONS'],
-    allowedHeaders:['Content-Type','Authorization','Accept','Origin','X-Requested-With'],
-  }));
-app.options('*',cors());
+
 
 newsRouter.get('/',getNews);
 newsRouter.post('/postNews',postNews);
