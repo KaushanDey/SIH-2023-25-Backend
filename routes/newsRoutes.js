7c9c0ff50239a8a47f8ff1b2c64661accc838954
KaushanDey <kaushandey2003@yahoo.com>
2023-09-27 17:13:10 +0530

    Changed news

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getNews, postNews } from "../controllers/newsController.js";
+import { getNews, getNewsByType, postNews } from "../controllers/newsController.js";
 
 
 const newsRouter = express.Router();
 
 newsRouter.get('/:district',getNews);
+newsRouter.get('/type/:type',getNewsByType);
 newsRouter.post('/postNews',postNews);
 
 export default newsRouter;
