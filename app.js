ffd538fe39cdcfdb900e2f249fb722344e41eb32
KaushanDey <kaushandey2003@yahoo.com>
2023-09-27 10:59:28 +0530

    Cors trial 3

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({
-  origin:"https://jade-mushy-lion.cyclic.cloud",
+  origin:"*",
   methods:["GET","POST","PUT"],
   allowedHeaders:['Content-Type','Authorization','Accept','Origin','X-Requested-With'],
 }));
