7035868c2e3e2b4b3cae9d17455e93bee6dfbda1
KaushanDey <kaushandey2003@yahoo.com>
2023-09-28 10:50:04 +0530

    jbdcbj

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import chatRouter from "./routes/chatRoutes.js";
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
